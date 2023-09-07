359060c7b42fd00648a44e93173f573353e3092a
melauderda42 <melauderda42@tntech.edu>
2023-09-06 22:25:49 -0500

    Navbar Layout completed

--- website/views.py
+++ website/views.py
@@ -8,11 +8,21 @@ def home():
     return render_template("base.html")
 
 
+@views.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+
 @views.route('/projects')
 def projects():
-    return "<p>My Projects:</p>"
+    return render_template("projects.html")
 
 
 @views.route('/about')
 def about_me():
     return render_template("about.html")
+
+
+@views.route('/contact')
+def contact():
+    return render_template("contact.html")
